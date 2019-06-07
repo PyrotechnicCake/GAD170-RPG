128c4336539e02b044acf96dbc1f8f7a68a3680d
Jake <1013479@student.sae.edu.au>
2019-06-07 15:59:32 +1000

    coded in level up scenario and growth rates for level 1-5

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -4,80 +4,69 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-   
-    //create lives
-    
-    public int playerLives = 3;
-    public int classSize = 25;
-    public bool classCounted;
+    // Default the player stats
+
+    int playerlvl;
+    int maxHP;
+    int HP;
+    int str;
+    int def;
+    int spd;
+    int luck;
+    int exp;
+    int growth1;
+
+    // Default the enemy stats
+
+    int enelvl;
 
     // Start is called before the first frame update
     void Start()
     {
-        ///HelloWorld();
-        ///IncreaseLives(2);
-        ///DisplayName("Jake", "Thomas");
-
-        //creat functions
-        ///mix("baked", 45, "cake");
-
-        ///if(Random.Range(successCalc,100) > 75)
+        growth1 = 20 + playerlvl * 10;
+        if (growth1 > 80)
+        {
+            growth1 = 80;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //level up the player based off chance
+        if(exp >= 100)
         {
-            //coin toss
-            if (Random.Range(1, 7) > 4)
-            {
-
-                IncreaseLives(2);
-                print("you win the coin toss.");
-            }
-            else
-            {
-                print("you loose the coin toss.");
-                IncreaseLives(-2);
-
-            }
+            LevelUp();
+            exp = 0;
         }
+    }
 
-        if (!classCounted)
+    void LevelUp()
+    {
+        //check if maxHP increases
+        if (Random.Range(0, 100) > growth1)
         {
-            for (int i = 0; i < classSize; i++)
-            {
-                print(i);
-            }
-            classCounted = true;
+            maxHP += 1;
         }
-
-    }
-
-    void HelloWorld()
-    {
-        print("Hello world!");
-    }
-
-    void IncreaseLives(int incLives)
-    {
-        playerLives += incLives;
-    }
-
-    void DisplayName(string FirstName, string Lastname)
-    {
-        print(FirstName + " " + Lastname);
-    }
-
-    void ingredients(int amA, string ingA, int amB, string ingB, int amC, string ingC)
-    {
-        print("There are " + amA + " " + ingA + ", " + amB + " " + ingB + " and " + amC + " " + ingC + ".");
-    }
-
-    void mix (string method, int time, string result)
-    {
-        ingredients(4, "eggs", 3, "tablespoons of sugar", 1, "cup of milk");
-        print("They where mixed together and " + method + " for " + time + " minutes to make a " + result + ".");
+        //check if str increases
+        if (Random.Range(0, 100) > growth1)
+        {
+            str += 1;
+        }
+        //check if def increases
+        if (Random.Range(0, 100) > growth1)
+        {
+            def += 1;
+        }
+        //check if spd increases
+        if (Random.Range(0, 100) > growth1)
+        {
+            spd += 1;
+        }
+        //check if luck increases
+        if (Random.Range(0, 100) > growth1)
+        {
+            luck += 1;
+        }
     }
 }
---
+++ Assets/rando.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rando : MonoBehaviour
+{
+   
+    //create lives
+    
+    public int playerLives = 3;
+    public int classSize = 25;
+    public bool classCounted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ///HelloWorld();
+        ///IncreaseLives(2);
+        ///DisplayName("Jake", "Thomas");
+
+        //creat functions
+        ///mix("baked", 45, "cake");
+
+        ///if(Random.Range(successCalc,100) > 75)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //coin toss
+            if (Random.Range(1, 7) > 4)
+            {
+
+                IncreaseLives(2);
+                print("you win the coin toss.");
+            }
+            else
+            {
+                print("you loose the coin toss.");
+                IncreaseLives(-2);
+
+            }
+        }
+
+        if (!classCounted)
+        {
+            for (int i = 0; i < classSize; i++)
+            {
+                print(i);
+            }
+            classCounted = true;
+        }
+
+    }
+
+    void HelloWorld()
+    {
+        print("Hello world!");
+    }
+
+    void IncreaseLives(int incLives)
+    {
+        playerLives += incLives;
+    }
+
+    void DisplayName(string FirstName, string Lastname)
+    {
+        print(FirstName + " " + Lastname);
+    }
+
+    void ingredients(int amA, string ingA, int amB, string ingB, int amC, string ingC)
+    {
+        print("There are " + amA + " " + ingA + ", " + amB + " " + ingB + " and " + amC + " " + ingC + ".");
+    }
+
+    void mix (string method, int time, string result)
+    {
+        ingredients(4, "eggs", 3, "tablespoons of sugar", 1, "cup of milk");
+        print("They where mixed together and " + method + " for " + time + " minutes to make a " + result + ".");
+    }
+}
