f9418da209004f511ad8e4bbf983227bc594b54d
Jake <1013479@student.sae.edu.au>
2019-06-14 11:44:55 +1000

    added enemy prefabs

---
+++ Assets/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //call my stats
+    Stats myStats;
+
+    //enemy enum
+    public enum Enemytypes
+    {
+        small,
+        medium,
+        large
+    }
+
+    public Enemytypes myType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStats = GetComponent<Stats>(); 
+        switch(myType)
+        {
+            case Enemytypes.small:
+                //do setup
+                break;
+            case Enemytypes.medium:
+                //setup
+                break;
+            case Enemytypes.large:
+                //setup
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myStats.maxHP = 4;
+    }
+
+    public void Attacked(int incDmg)
+    {
+        myStats.HP -= incDmg - myStats.def;
+    }
+}
---
+++ Assets/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    //Initialize stats for everyone!! YAY!!!
+    public int maxHP;
+    public int HP;
+    public int str;
+    public int def;
+    public int spd;
+    public int luck;
+}
